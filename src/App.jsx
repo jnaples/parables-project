0ba5d22e18c35a44d75feca7dc8640c0287dd4e0
“Joe <jnaples90@gmail.com>
2026-04-25 07:07:45 -0400

    push code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-[500px] w-full items-center justify-center bg-[url('/images/hero.png')] bg-cover bg-no-repeat">
+        <div className="container mx-auto p-6 text-center text-white">
+          <h1 className="text-5xl">The</h1>
+          <h1 className="text-[64px] leading-tight">Parables of Jesus</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
